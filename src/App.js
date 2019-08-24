fe0effbf44d9efc287fb501da9d21b7e3d835162
Dan Kreiger <dan@dankreiger.com>
2019-08-24 19:24:58 +0200

    chore(*): react router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './components/Toggle/Toggle';
-//import Nav from './components/Nav/Nav';
-import Checkout from './components/Checkout/Checkout';
+import Routes from './Routes';
 
 const App = () => {
   const [isNavOpen, setNavOpen] = useState(false);
-  // const navAnimation = useSpring({
-  //   transform: isNavOpen ? `translate3d(0, 0,0)` : `translate3d(100%, 0,0)`
-  // });
   const fade = useSpring({
     from: {
       opacity: 0
@@ -24,11 +19,9 @@ const App = () => {
         <button onClick={() => setNavOpen(!isNavOpen)} className="menu-button">
           Menu
         </button>
-        {/* <Nav navAnimation={navAnimation} /> */}
       </header>
       <main>
-        <Toggle />
-        <Checkout isOpen={isNavOpen} />
+        <Routes />
       </main>
     </animated.div>
   );
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -3,23 +3,70 @@ import { useTransition, animated } from 'react-spring';
 //import PropTypes from 'prop-types';
 
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const transition = useTransition(isToggled, null, {
+  const [items, setItems] = useState([
+    {
+      letter: 'W',
+      key: 1
+    },
+    {
+      letter: 'a',
+      key: 2
+    },
+    {
+      letter: 'u',
+      key: 3
+    },
+    {
+      letter: 's',
+      key: 4
+    },
+    {
+      letter: 't',
+      key: 5
+    },
+    {
+      letter: 'e',
+      key: 6
+    },
+    {
+      letter: 'r',
+      key: 7
+    }
+  ]);
+
+  // useTransition mounts and unmounts items
+  const transition = useTransition(items, item => item.key, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 }
   });
   return (
     <div>
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
-      {transition.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.h1 key={key} style={props}>
-              Hello
-            </animated.h1>
-          )
-      )}
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: 'D',
+              key: 1
+            },
+            {
+              letter: 'O',
+              key: 2
+            },
+            {
+              letter: 'G',
+              key: 3
+            }
+          ])
+        }
+      >
+        Toggle
+      </button>
     </div>
   );
 };
@@ -29,3 +76,22 @@ const Toggle = () => {
 Toggle.propTypes = {};
 
 export default Toggle;
+
+// const [isToggled, setToggle] = useState(false);
+
+// const transition = useTransition(isToggled, null, {
+//   from: { opacity: 0, position: 'absolute' },
+//   enter: { opacity: 1 },
+//   leave: { opacity: 0 }
+// });
+
+// <div style={{ position: 'relative' }}>
+// <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+// {transition.map(({ item, key, props }) =>
+//   item ? (
+//     <animated.h1 style={props}>The english dog barks</animated.h1>
+//   ) : (
+//     <animated.h1 style={props}>The German dog wauwaus</animated.h1>
+//   )
+// )}
+// </div>
