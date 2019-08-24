bea2c78b0175bb356cddfa0efa132bfc4bdd1943
Dan Kreiger <dan@dankreiger.com>
2019-08-24 18:49:21 +0200

    chore(*): use transition

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,32 +1,25 @@
 import React, { useState } from 'react';
-// eslint-disable-next-line
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 //import PropTypes from 'prop-types';
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
-  const { color, y } = useSpring({
-    // opacity: isToggled ? 1 : 0,
-    // fontSize: isToggled ? '2rem' : '5em',
-    color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : -50
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   });
   return (
     <div>
-      <animated.h1
-        style={{
-          transform: y
-            .interpolate({
-              range: [0, 0.25, 0.5, 0.75, 1],
-              output: [0, -25, -50, -100, -50]
-            })
-            .interpolate(y => `translate3d(0,${y}px,0)`),
-          color
-        }}
-      >
-        Hello
-      </animated.h1>
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.h1 key={key} style={props}>
+              Hello
+            </animated.h1>
+          )
+      )}
     </div>
   );
 };
